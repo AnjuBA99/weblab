458ceb0fbc0a4bffd473263f774672667e23df1f
AnjuBA99 <75128330+AnjuBA99@users.noreply.github.com>
2021-03-21 13:11:33 +0530

    Add files via upload

---
+++ dbconnect.php
@@ -0,0 +1,18 @@
+<?php
+
+$conn=mysqli_connect("localhost","root","","test");
+
+if(mysqli_connect_errno())
+
+{
+
+	echo "Connection Error:".mysqli_connect_errno();
+
+	exit();
+
+}
+
+
+
+
+?>
---
+++ web12.php
@@ -0,0 +1,37 @@
+<?php
+
+include("dbconnect.php");
+
+$sql="SELECT * FROM student";
+
+$result=$conn->query($sql);
+
+if($result->num_rows>0)
+
+   {
+
+	echo"<table border='1' width='500px';style='border:5px solid black;margin-left:400;margin-top:100px;border-collapse:collapse;'>
+
+	<tr><th>ID</th><th>Name</th><th>Email</th><th>Marks</th></tr>";
+
+	while($row=$result->fetch_assoc())
+	
+           {
+
+		echo "<tr><td>".$row["id"]."</td>";
+	
+	        echo "<td>".$row["name"]."</td>";
+	
+	        echo "<td>".$row["email"]."</td>";
+	
+	        echo "<td>".$row["marks"]."</td></tr>";
+
+	  }
+	
+
+  }
+
+echo"</table>";
+
+
+?>
